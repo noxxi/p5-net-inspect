de6042eb288ab0267895beb20f68fb9f08560698
Steffen Ullrich <github@maulwuff.de>
2011-12-19 13:39:53 +0100

    0.01 initial release

---
+++ bin/http_inspection_proxy.pl
@@ -0,0 +1,664 @@
+#!/usr/bin/perl
+
+=pod
+
+TODO:
+- work transparently, e.g. with iptables REDIRECT rules, instead of 
+  parsing Host header
+- more and useful filters
+- ...
+
+=cut
+
+use strict;
+use warnings;
+use Net::Inspect::Debug qw($DEBUG debug %TRACE);
+use Net::Inspect::L7::HTTP;
+use Net::Inspect::L7::HTTP::Request::InspectChain;
+use Getopt::Long qw(:config posix_default bundling);
+use IO::Socket::INET6;
+#use Net::PcapWriter; - on Demand
+use AnyEvent;
+
+sub usage {
+	print STDERR <<USAGE;
+
+Proxy to forward after inspecting/modifying request and response.
+Current setup just adds 'X-Custom-Header' to response header, but removes
+unchunking and compression.
+
+$0 Options* [ip:port]+
+Options:
+  -h|--help        show usage
+  -d|--debug       debug mode
+  -T|--trace T     enable traces
+  -P|--pcapdir D   save connections as pcap files into D
+  --filter F       add named filter/modifier, can be used multiple times
+
+USAGE
+	exit(2);
+}
+
+my (@filter,$pcapdir);
+GetOptions(
+	'd|debug' => \$DEBUG,
+	'T|trace=s' => sub { $TRACE{$_} = 1 for split(m/,/,$_[1]) },
+	'h|help' => sub { usage() },
+	'P|pcapdir=s' => \$pcapdir,
+	'filter=s' => \@filter,
+);
+my @addr = @ARGV or usage("no listener");
+die "pcap directory not writeable " if $pcapdir and not -d $pcapdir && -w _;
+require 'Net::PcapWriter' if $pcapdir;
+
+main();
+############################################################################
+# Connection object
+# derived from Net::Inspect::L7::HTTP
+# just pipes all input into conn->in(...)
+############################################################################
+
+package myHTTPConn;
+use base 'Net::Inspect::L7::HTTP';
+use Hash::Util 'lock_keys';
+use Net::Inspect::Debug;
+use Scalar::Util 'weaken';
+use fields (
+	'fds',         # array of fos: 0 for client, 1 for server connection
+	'upstream',    # current upstream host
+	'spool',       # any data which cannot be processed yet?
+	'pcapw',       # Net::PcapWriter
+	'didit',       # timestamp for last read|write
+);
+
+
+# active connections, inserted at new_connection, removed in idlet
+my @active_connections;
+
+my $idlet = AnyEvent->timer( after => 5, interval => 5, cb => sub { 
+		@active_connections = grep { $_ } @active_connections 
+			or return;
+		debug("check timeouts for %d conn",+@active_connections);
+		my $now = AnyEvent->now;
+		for (@active_connections) {
+			$_ or next;
+			$_->{didit} + 30 > $now and return;
+			$_->Close 
+		}
+	}
+);
+	
+sub DESTROY { shift->xdebug("connection done"); }
+sub new_connection {
+	my ($self,$fd) = @_;
+	my $obj = $self->SUPER::new_connection({
+		daddr => $fd->sockhost,
+		dport => $fd->sockport,
+		saddr => $fd->peerhost,
+		sport => $fd->peerport,
+	});
+	$obj->newfo(0,$fd);
+	readmask($obj,0,1);
+	$obj->{didit} = AnyEvent->now;
+
+	if ( $pcapdir ) {
+		open( my $fh,'>', sprintf("%s/%d.%d.pcap",$pcapdir,$$,$obj->{connid}))
+			or die "cannot open pcap file: $!";
+		$fh->autoflush;
+		my $w = Net::PcapWriter->new($fh);
+		my $c = $w->tcp_conn( $fd->sockhost, $fd->sockport, $fd->peerhost, $fd->peerport );
+		$obj->{pcapw} = [$c,$w],
+	}
+
+
+	push @active_connections,$obj;
+	weaken($active_connections[-1]);
+
+	return $obj;
+}
+
+sub fatal {
+	my ($self,$reason) = @_;
+	$reason = "$$.$self->{connid} $reason";
+	warn "[fatal] $reason\n";
+	$self->Close;
+}
+
+sub in {
+	my $self = shift;
+	return $self->SUPER::in(@_) if ! $self->{pcapw};
+	my ($dir,$data,$eof,$time) = @_;
+	if ( defined ( my $bytes = eval { $self->SUPER::in(@_) } )) {
+		$self->{pcapw}[0]->write($dir,substr($data,0,$bytes));
+		return $bytes;
+	} else {
+		# save final data
+		$self->{pcapw}[0]->write($dir,$data);
+		die $@ if $@;
+		return;
+	}
+}
+
+sub readmask {
+	my ($self,$dir,$mask) = @_;
+	my $fo = $self->{fds}[$dir] or return;
+	if ( $mask ) {
+		$fo->{status} & 0b100 and return 0; # read shutdown already
+		$fo->{rwatch} and return 1;
+		$fo->{rsub} ||= sub { _read($self,$dir) };
+		$fo->{rwatch} = AnyEvent->io( 
+			fh => $fo->{fd}, 
+			poll => 'r', 
+			cb => $fo->{rsub}
+		);
+	} else {
+		undef $fo->{rwatch};
+	}
+	return 1;
+}
+	
+sub _read {
+	my ($self,$dir) = @_;
+	my $fo = $self->{fds}[$dir] or return;
+	my $n = sysread($fo->{fd},$fo->{rbuf},2**15,length($fo->{rbuf}));
+	if ( ! defined $n ) {
+		$self->fatal("read($dir) failed: $!");
+		return;
+	}
+	if ( ! $n ) {
+		# eof 
+		$self->xdebug("read shutdown $dir");
+		undef $fo->{rwatch};
+		undef $fo->{rsub};
+		$fo->{status} |= 0b100; # read-shutdown
+		shutdown($fo->{fd},0);
+		$self->_closeIfDone and return;
+	}
+
+	$self->{didit} = AnyEvent->now;
+	if ( my $bytes = $self->in($dir,$fo->{rbuf},!$n,AnyEvent->now)) {
+		substr($fo->{rbuf},0,$bytes,'');
+	}
+	my $odir = $dir?0:1;
+	if ( my $ofo = $self->{fds}[$odir] ) {
+		if ( $ofo->{wbuf} ne '' ) {
+			_write($self,$odir);
+			# disable read until all data are written to the other side
+			if ( $ofo->{wbuf} ne '' ) {
+				_writemask($self,$odir,1);
+				undef $fo->{rwatch};
+				return;
+			}
+		}
+
+		# wbuf empty, if eof close writer too
+		if ( ! $n ) {
+			$self->xdebug("write shutdown $odir");
+			undef $fo->{wwatch};
+			undef $fo->{wsub};
+			$ofo->{status} |= 0b010; # write shutdown
+			shutdown($ofo->{fd},1);
+			$self->_closeIfDone and return;
+		}
+	}
+}
+
+sub _closeIfDone {
+	my $self = shift;
+	my $fds = $self->{fds} or return 1;
+	if ( ! $fds->[0] || $fds->[0]{status} & 0b110 == 0b110
+		and ! $fds->[1] || $fds->[1]{status} & 0b110 == 0b110 ) {
+		$self->Close;
+		return 1;
+	}
+	return;
+}
+
+sub _writemask {
+	my ($self,$dir,$mask) = @_;
+	my $fo = $self->{fds}[$dir] or return;
+	if ( $mask ) {
+		$fo->{wwatch} and return;
+		$fo->{wsub} ||= sub { $self->_write($dir) };
+		$fo->{wwatch} = AnyEvent->io( 
+			fh => $fo->{fd}, 
+			poll => 'w', 
+			cb => $fo->{wsub}
+		);
+	} else {
+		undef $fo->{wwatch};
+	}
+}
+
+sub Send {
+	my ($self,$dir,$data) = @_;
+	my $fo = $self->{fds}[$dir] or return; # socket down
+	$fo->{wbuf} .= $data;
+	_write($self,$dir);
+}
+
+sub Close {
+	my $self = shift;
+	$self->xdebug("closing");
+	for (@{$self->{fds}}) {
+		$_->{wwatch} = $_->{rwatch} = $_->{wsub} = $_->{rsub} = undef;
+		close($_->{fd}) if $_->{fd};
+		$_->{fd} = undef;
+	}
+	undef $self->{fds};
+}
+
+sub _write {
+	my ($self,$dir) = @_;
+
+	my $fo = $self->{fds}[$dir] or return;
+	my $n = syswrite($fo->{fd},$fo->{wbuf});
+	if ( ! defined $n ) {
+		$self->fatal("write($dir) failed: $!");
+		return;
+	}
+
+	$self->{didit} = AnyEvent->now;
+
+	substr($fo->{wbuf},0,$n,'');
+	if ( $fo->{wbuf} eq '' ) {
+		# nothing more to write
+		$self->xdebug("all written to $dir");
+		undef $fo->{wwatch};
+		# enable read again
+		if ( ! readmask($self,$dir?0:1,1) ) {
+			# already shutdown - propagate
+			shutdown($fo->{fd},1);
+			$fo->{status} |= 0b010; # write shutdown
+			$self->xdebug("write shutdown $dir");
+			undef $fo->{wsub};
+			$self->_closeIfDone and return;
+		}
+	}
+}
+
+
+sub newfo {
+	my ($self,$dir,$fd,$status) = @_;
+	my %fo = ( 
+		fd => $fd, 
+		# read shutdown|write shutdown|connected
+		status => defined($status) ? $status : 0b001, # connected
+		rbuf => '', rsub => undef, rwatch => undef, 
+		wbuf => '', wsub => undef, wwatch => undef,
+	);
+	lock_keys(%fo);
+	$self->{fds}[$dir] = \%fo
+}
+
+sub dump_state {
+	my $self = shift;
+	if ( my $fds = $self->{fds} ) {
+		my @st;
+		for( my $i=0;$i<@$fds;$i++) {
+			push @st, sprintf("%d=%03b",$i,$fds->[$i]{status} || 0);
+		}
+		$self->xdebug("status @st");
+	};
+	$self->SUPER::dump_state;
+}
+
+
+############################################################################
+# Request
+############################################################################
+package myHTTPRequest;
+use base 'Net::Inspect::L7::HTTP::Request::InspectChain';
+use fields qw(chunked);
+use Net::Inspect::Debug qw(debug $DEBUG);
+use AnyEvent::Socket qw(tcp_connect format_address);
+
+
+sub in_request_header {
+	my $self = shift;
+
+	# if there is a request still open spool call
+	if ( my $spool = $self->{conn}{spool} ) {
+		push @$spool,['in_request_header',@_];
+		# disable read - other calls might still be spooled because
+		# they origin from the same packet (post data, pipelining..)
+		$self->readmask(0,0);
+		return 1;
+	}
+
+	my ($hdr,$time) = @_;
+	my ($method) = $hdr =~m{^(\w+)};
+	if ( $method !~ m{^(?:GET|POST|PUT|DELETE|TRACE|OPTIONS|CONNECT)$} ) {
+		$self->fatal("cannot handle method $method");
+		return 1;
+	}
+
+	$hdr =~s{^Proxy-Connection:}{Connection:}im; #  
+	$self->add_hooks({ 
+		name => 'fwd-data',
+		request_header => \&_connect_upstream,
+		request_body => sub { 
+			my ($self,$data,$eof,$time) = @_;
+			if ( $$data ne '' ) {
+				$self->{conn}->Send(1,$$data);
+				$$data = '';
+			}
+			return '';
+		},
+		response_header => sub { 
+			my ($self,$hdr,$time) = @_;
+			$self->{conn}->Send(0,$$hdr);
+			return 0;
+		},
+		response_body => sub { 
+			my ($self,$data,$eof,$time) = @_;
+			$self->{conn}->Send(0,$$data) if $$data ne '';
+			$$data = '';
+			return '';
+		},
+		chunk_header => sub {
+			my ($self,$hdr,$time) = @_;
+			return if $$hdr eq '';
+			$self->{conn}->Send(0, $self->{chunked}++ ? "\r\n$$hdr" : $$hdr );
+			return 1;
+		},
+		chunk_trailer => sub {
+			my ($self,$trailer,$time) = @_;
+			return if $$trailer eq '';
+			$self->{conn}->Send(0, $$trailer );
+			return 1;
+		},
+	});
+
+	$self->{conn}{spool} ||= [];
+	return $self->SUPER::in_request_header($hdr,$time);
+}
+
+sub in_request_body {
+	my $self = shift;
+	my $conn = $self->{conn};
+
+	# if there is a request still open spool call
+	# at least in_request_header must have been spooled
+	my $spool = $conn->{spool};
+	if ( $spool && @$spool ) { 
+		$self->xdebug("spooling request body");
+		push @$spool,['in_request_body',@_];
+		return 1;
+
+	# post data but not yet connected
+	} elsif ( ! $conn->{fds}[1] ) {
+		#$self->xdebug("spooling postdata because not connected yet");
+		$conn->readmask(0,0);
+		$conn->{spool} ||= [];
+		push @$spool,['in_request_body',@_];
+		return 1;
+	}
+
+	return $self->SUPER::in_request_body(@_);
+}
+
+sub _call_spooled {
+	my ($self,$what) = @_;
+	my $spool = $self->{conn}{spool} or return;
+	$self->{conn}{spool} = undef;
+
+	while ( @$spool and ! $self->{conn}{spool} ) {
+		my ($method,@arg) = @{ shift(@$spool) };
+		if ( ! $what or $what->{$method} ) {
+			$self->$method(@arg);
+		} else {
+			unshift @$spool,[$method,@arg];
+			last;
+		}
+	}
+
+	# put unfinished requests back into spool
+	unshift @{ $self->{conn}{spool} }, @$spool if @$spool;
+
+	# enable read again if nothing in spool
+	$self->{conn}->readmask(0,1) if ! $self->{conn}{spool};
+}
+
+sub in_response_body {
+	my ($self,$data,$eof,$time) = @_;
+	my $rv = $self->SUPER::in_response_body($data,$eof,$time);
+	if ( $eof ) {
+		$self->xdebug("got eof in response");
+		my $rphdr = $self->response_header;
+		if ( ! $rphdr->header('Content-length') and
+			($rphdr->header('Transfer-Encoding')||'') !~m{\bchunked\b} ) {
+			# no content-length given etc, probably because of decompression
+			# need to close connection at response end to signal end of
+			# response
+			$self->xdebug("closing connection");
+			$self->{conn}->Close;
+			return $rv;
+		} else {
+			#$self->xdebug("response header: ".$rphdr->as_string);
+		}
+
+		# any more spooled requests (pipelining)?
+		_call_spooled($self);
+	}
+	return $rv
+}
+
+sub in_data {
+	my ($self,$dir,$data,$eof,$time) = @_;
+	# forward data to other side
+	$self->xdebug("%s bytes from %s to %s",length($data),$dir,$dir?0:1);
+	$self->{conn}->Send($dir?0:1,$data) if $data ne '';
+	return length($data);
+}
+
+sub _connect_upstream {
+	my ($self,$hdr,$time) = @_;
+	my $req = $self->request_header;
+	my $uri = $req->uri;
+
+	my $hdr_changed = 0;
+	my ($proto,$host,$port,$page);
+	if ( $req->method eq 'CONNECT' ) {
+		($host,$port) = $uri =~m{^(.*?)(?:\:(\d+))?$};
+		$port ||= 443;
+		$host =~s{^\[(.*)\]$}{$1};
+		$proto = 'https';
+		$page = '';
+		# dont' forward anything, but don't change header :)
+		$hdr = \( '' );
+	} else {
+		($proto,$host,$port,$page) = ($1,$2,$3||80,$4)
+			if $uri =~m{^(?:(\w+)://([^/\s]+?)(?::(\d+))?)?(/.*|$)};
+		($proto,$host,$port) = ('http',$2,$3||80) 
+			if ! $proto and
+			$req->header('Host') =~m{^(\S+?)(?:(\d+))?$};
+		$proto or return $self->fatal('bad request: '.$$hdr),undef;
+		return $self->fatal("bad proto: $proto"),undef
+			if $proto ne 'http';
+
+		# make method://host/page to /page
+		$hdr_changed = 1 if $$hdr =~s{\A(\w+[ \t]+)(\w+://[^/]+)}{$1};
+	}
+
+	$self->xdebug("new request $proto://$host:$port$page");
+
+	$host =~s{^\[(.*)\]$}{$1}; # ipv6
+	if ( $self->{conn}{upstream} and
+		$self->{conn}{upstream} ne "$host.$port") {
+		$self->xdebug("upstream host changed from $self->{conn}{upstream} to $host.$port");
+		undef $self->{conn}{upstream};
+		undef $self->{conn}{fds}[1];
+	}
+
+	# check if we have the connection still open,
+	# otherwise connect
+	if ( ! $self->{conn}{fds}[1] ) {
+		$self->xdebug("connecting to $host.$port");
+		# async dns lookup + connect
+		myDNS::lookup($host, sub {
+			$self->{conn} or return; # already closed
+			if ( my $addr = shift ) {
+				tcp_connect($addr,$port, sub {
+					if ( my $fd = shift ) {
+						if ( $self->{conn} ) {
+							$self->{conn}{upstream} = "$host.$port";
+							$self->{conn}->newfo(1,$fd);
+							$self->{conn}->readmask(1,1);
+							$self->xdebug("connect done");
+							_connect_upstream_connected($self,$hdr,$time);
+						}
+					} else {
+						myDNS::uncache($host,$addr);
+						$self->fatal("connect to $host.$port failed: $!");
+						return;
+					}
+				});
+			} else {
+				$self->fatal("connect to $host.$port failed: no such host (DNS)");
+			}
+		});
+	} else {
+		_connect_upstream_connected($self,$hdr,$time);
+	}
+	return $hdr_changed;
+}
+
+sub _connect_upstream_connected {
+	my ($self,$hdr,$time) = @_;
+
+	if ( $DEBUG ) {
+		my $cfd = $self->{conn}{fds}[0]{fd};
+		my $sfd = $self->{conn}{fds}[1]{fd};
+		my $gethost = sub {
+			my ($s,$h) = AnyEvent::Socket::unpack_sockaddr(shift || return 'undef');
+			return format_address($h).".$s";
+		};
+		$self->xdebug( "request from %s | %s to %s | %s",
+			$gethost->(getpeername($cfd)),
+			$gethost->(getsockname($cfd)),
+			$gethost->(getsockname($sfd)),
+			$gethost->(getpeername($sfd)),
+		);
+	}
+
+	if ( $$hdr ne '' ) {
+		$self->{conn}->Send(1,$$hdr);
+	} else {
+		# successful Upgrade, CONNECT.. - send OK to client
+		# fake that it came from server so that the state gets
+		# maintained in Net::Inspect::L7::HTTP
+		$self->{conn}->in(1,"HTTP/1.0 200 OK\r\n\r\n",0,$time);
+	}
+
+	# any more spooled request bodys?
+	_call_spooled($self,{ in_request_body => 1 });
+
+	return 0; # not modified
+}
+
+sub fatal {
+	my ($self,$reason) = @_;
+	my $conn = $self->{conn};
+	if ( $conn ) {
+		my $reqid = $self->{conn}{meta} && $self->{conn}{meta}{reqid} // '';
+		$reason = "$$.$self->{conn}{connid}.$reqid $reason";
+		$self->{conn}->Close;
+	}
+	warn "[fatal] $reason\n";
+}
+
+############################################################################
+# DNS cache
+############################################################################
+package myDNS;
+use AnyEvent::DNS;
+use Socket qw(AF_INET AF_INET6 inet_pton);
+
+my %cache;
+sub uncache {
+	my ($host,$addr) = @_;
+	my $e = $cache{lc($host)} or return;
+	@$e = grep { $_ ne $addr } @$e;
+	delete $cache{lc($host)} if !@$e;
+}
+
+sub lookup {
+	my ($host,$cb) = @_;
+	$host = lc($host);
+
+	if ( my $e = $cache{$host} ) {
+		return $cb->(@$e);
+	} elsif ( inet_pton(AF_INET,$host) || inet_pton(AF_INET6,$host) ) {
+		return $cb->($host);
+	}
+
+	AnyEvent::DNS::a($host,sub {
+		if ( @_ ) {
+			$cache{$host} = [ @_ ];
+			return $cb->(@_);
+		}
+
+		# try AAAA
+		AnyEvent::DNS::aaaa($host,sub {
+			$cache{$host} = [ @_ ] if @_;
+			return $cb->(@_);
+		});
+	});
+}
+
+############################################################################
+# MAIN
+############################################################################
+package main;
+sub main {
+	# create connection fabric, attach request handling
+	my $req  = myHTTPRequest->new;
+	my $conn = myHTTPConn->new($req); 
+	$req->add_hooks('unchunk','uncompress_te','uncompress_ce');
+
+	# hooks adds custom header to each response
+	$req->add_hooks({
+		name => 'custom-header',
+		response_header => sub {
+			my ($self,$hdr) = @_;
+			my $rsp = $self->response_header;
+			$rsp->header( 'X-Custom-Header' => 'abcde' );
+			$$hdr = $rsp->as_string;
+			return 1;
+		},
+	});
+
+	# create listeners
+	my @listen;
+	for my $addr (@addr) {
+		my $fd = IO::Socket::INET6->new(
+			LocalAddr => $addr,
+			Listen    => 10,
+			Reuse     => 1,
+		) or die "cannot listen to $addr: $!";
+		push @listen, AnyEvent->io( 
+			fh => $fd, 
+			poll => 'r', 
+			cb => sub { 
+				my $cl = $fd->accept or return;
+				$conn->new_connection($cl);
+			}
+		);
+	}
+
+	my $sw = AnyEvent->signal( signal => 'USR1', cb => sub {
+		debug("-------- active connections -------------");
+		for(@active_connections) {
+			$_ or next;
+			$_->dump_state;
+		}
+		debug("-----------------------------------------");
+	});
+
+
+	# Mainloop
+	my $loopvar = AnyEvent->condvar;
+	$loopvar->recv;
+	exit;
+}
---
+++ lib/Net/Inspect/L4/TCP.pm
@@ -0,0 +1,368 @@
+############################################################################
+# TCP
+# opens und closes connections and feeds data into them
+# reorders packets based on sequence number
+############################################################################
+use strict;
+use warnings;
+package Net::Inspect::L4::TCP;
+use base 'Net::Inspect::Flow';
+use fields qw(conn);
+use Net::Inspect::Debug qw( debug trace );
+
+# field conn
+# hash indexed by {saddr,sport,daddr,dport} with values
+# [ \@d0,\@d1,$conn ]
+#   $conn - connection object
+#   \@d0,\@d1 - information about data from client(0) or server(1).
+#      [$sn,\%pkt,$buf,$state]
+#        $sn - all packets received up to this sequence number
+#        \%pkt - packets which are not added to $buf (out of order, missing
+#          packet in between).
+#        $buf - data which are not yet forwarded to attached flows
+#        $state: bitmask ( 0b0000FfSs : Fin+ack|Fin|Syn+ack|Syn)
+
+sub new {
+    my ($class,$flow) = @_;
+    my $self = $class->SUPER::new($flow);
+    $self->{conn} = {};
+    return $self;
+}
+
+sub pktin {
+    my Net::Inspect::L4::TCP $self = shift;
+    my ($pkt,$meta) = @_;
+    return if $meta->{proto} != 6; # does only handle tcp
+
+    # extract TCP header
+    my ($sport,$dport,$sn,$asn,$doc,$window,$cksum,$up)
+	= unpack('nnNNnnnn',$pkt);
+
+    # payload
+    my $do = ( $doc >> 12 )*4;
+    my $buf = substr($pkt,$do);
+
+    # flags
+    my $urg = $doc & 0b100000 && 1;
+    my $ack = $doc & 0b010000 && 1;
+    my $psh = $doc & 0b001000 && 1;
+    my $rst = $doc & 0b000100 && 1;
+    my $syn = $doc & 0b000010 && 1;
+    my $fin = $doc & 0b000001 && 1;
+
+    debug("%s%s%ssn=%u len=%d",
+	$syn ? 'SYN ':'',
+	$fin ? "FIN ":'',
+	$ack ? "ACK=$asn ":'',
+	$sn,
+	length($buf));
+
+    my $saddr = $meta->{saddr};
+    my $daddr = $meta->{daddr};
+
+    # find or create connection
+    my ($conn,$dir);
+    if ( $conn = $self->{conn}{$saddr,$sport,$daddr,$dport} ) {
+	$dir = 0;
+	debug("found conn $conn $saddr.$sport(%04b) -> $daddr.$dport(%04b)",
+	    $conn->[0][3],$conn->[1][3] );
+    } elsif ( $conn = $self->{conn}{$daddr,$dport,$saddr,$sport} ) {
+	$dir = 1;
+	# saddr should point to client all time..
+	($saddr,$sport,$daddr,$dport) = ($daddr,$dport,$saddr,$sport);
+	debug("found conn $conn $saddr.$sport(%04b) <- $daddr.$dport(%04b)",
+	    $conn->[0][3],$conn->[1][3] );
+    } else {
+	$dir = 0
+    }
+
+    # new connection, ISN = $sn
+    if ( $syn ) {
+	if ( $fin ) {
+	    # invalid combination
+	    trace("FIN+SYN($dir) for connection $saddr.$sport -> $daddr.$dport - ignoring");
+	    return 1;
+	}
+
+	debug("SYN($dir) for new flow $saddr.$sport -> $daddr.$dport");
+	if ( $conn and $conn->[$dir][3] & 0b0001 ) {
+	    # SYN already received, duplicate?
+	    trace("duplicate SYN($dir) for connection $saddr.$sport -> $daddr.$dport - ignoring");
+	    return 1;
+	}
+	if ( ! $conn ) {
+	    my $rv = $self->syn({
+		time  => $meta->{time},
+		saddr => $saddr,
+		sport => $sport,
+		daddr => $daddr,
+		dport => $dport,
+		dir   => $dir,
+	    });
+	    if ( defined $rv && ! $rv ) {
+		# we don't want a connection for this
+		debug("ignored syn $saddr.$sport -> $daddr.$dport because of syn-hook");
+		return 1;
+	    }
+	}
+
+	# dok for structure see top of file
+	if ( ! $conn ) {
+	    $conn = [ [ undef,{},'',0], [ undef,{},'',0], undef ];
+	    # register new connection
+	    debug("register conn $saddr.$sport -> $daddr.$dport $conn");
+	    $self->{conn}{$saddr,$sport,$daddr,$dport} = $conn;
+	}
+
+	# set ISN and state for direction
+	$conn->[$dir][0] = $sn+1;
+	$conn->[$dir][3] = 0b0001;
+    }
+
+    # must have $conn here or ignore packet
+    if ( ! $conn ) {
+	debug("no established connection $saddr.$sport <-> $daddr.$dport");
+	return 1;
+    }
+
+    if ( $buf ne '' or $fin ) {
+
+	# add buf to packets
+	$conn->[$dir][1]{$sn} = $buf if $buf ne '';
+
+	# set FIN flag - no more data expected from peer after this point
+	# but outstanding packets might still come in
+	if ( $fin ) {
+	    if ( not $conn->[$dir][3] & 0b0100 ) {
+		debug("shutdown dir $dir $saddr.$sport -> $daddr.$dport");
+		$conn->[$dir][3] |= 0b0100; # fin received
+		# must increase sn for ACK
+		$conn->[$dir][1]{ ( $sn+length($buf) ) % 2**32  } = undef;
+	    } else {
+		# probably duplicate
+		debug("ignore duplicate FIN($dir) $saddr.$sport -> $daddr.$dport");
+		return 1;
+	    }
+	}
+    }
+
+    if ( $ack ) {
+	my $odir = $dir?0:1;
+
+	# reorder and concat packets up to acknowledged value and forward
+	# to attached flows
+	my $pkts = $conn->[$odir][1];
+	if ( %$pkts ) {
+	    my @buf;
+	    my $xsn = $conn->[$odir][0];
+	    my $eof = 0;
+	    while ( $xsn != $asn and exists $pkts->{$xsn} ) {
+		my $pkt = delete $pkts->{$xsn};
+		if ( defined $pkt ) {
+		    push @buf,$pkt;
+		    $xsn = ( $xsn + length($pkt)) % 2**32;
+		} else {
+		    # undef means FIN, there should be no more packets
+		    # after this
+		    $xsn = ( $xsn + 1 ) % 2**32;
+		    $eof = 1;
+		    debug("got ACK for FIN($odir)");
+		    last;
+		}
+	    }
+
+	    # lost packets or ack points in the middle of packet
+	    if ( $xsn != $asn ) {
+		trace("lost packets before ACK($odir)=$asn $saddr.$sport -> $daddr.$dport");
+		delete $self->{conn}{$saddr,$sport,$daddr,$dport};
+		if ( my $obj = $conn->[2] ) {
+		    $obj->fatal( "lost packets before ACK($odir)=$asn",
+			$meta->{time});
+		}
+		return 1;
+	    }
+
+	    # data after fin
+	    if ( $eof and %$pkts ) {
+		trace("ignoring packets after FIN($odir) $saddr.$sport -> $daddr.$dport");
+		%$pkts = ();
+		return 1;
+	    }
+
+	    # update sn etc
+	    $conn->[$odir][0] = $xsn;
+	    $conn->[$odir][2] .= join('',@buf);
+	    $conn->[$odir][3] |= 0b1000 if $eof;
+
+	    # forward data
+	    if ( my $obj = $conn->[2] ) {
+		# set eof to 2 to signal that both sides are closed
+		$eof = 2 if $eof and $conn->[$dir][3] & 0b1000;
+		my $n = $obj->in($odir,$conn->[$odir][2],$eof,$meta->{time});
+		if ( ! defined $n ) {
+		    # error processing -> close
+		    # don't call fatal, hook probably reported error already
+		    trace("error processing data in hook in $saddr.$sport -> $daddr.$dport");
+		    delete $self->{conn}{$saddr,$sport,$daddr,$dport};
+		    return 1;
+		} elsif ( $n>0 ) {
+		    # strip all processed bytes from buffer
+		    substr($conn->[$odir][2],0,$n) = '';
+		    debug("keep %d bytes in buffer for $odir",length($conn->[$odir][2]))
+			if length($conn->[$odir][2]);
+		}
+	    }
+
+	    # eof: if other side fin+acked too reap connection
+	    if ( $eof and $conn->[$dir][3] & 0b1000 ) {
+		debug("connection  $saddr.$sport -> $daddr.$dport closed");
+		delete $self->{conn}{$saddr,$sport,$daddr,$dport};
+		return 1;
+	    }
+	}
+
+	# check for ACK to SYN
+	my $osn = $conn->[$odir][0];
+	if ( ! defined $osn ) {
+	    # got ack w/o syn?
+	    trace("received ACK w/o SYN for dir=$odir $saddr.$sport -> $daddr.$dport");
+	    return 1;
+	}
+
+	# ACK for SYN?
+	if ( not $conn->[$odir][3] & 0b0010 ) {
+	    # check that ACK matches ISN from SYN
+	    if ( $osn % 2**32 != $asn ) {
+		trace("got ACK($asn) which does not match SYN($osn)");
+		return 1;
+	    }
+	    # ack ok
+	    $conn->[$odir][3] |= 0b0010;
+	    debug("got ACK for SYN($odir)");
+
+	    # got other side syn+ack too? -> new connection
+	    if ( $conn->[$dir][3] & 0b0010 ) {
+		# make connection object
+		my $obj = $self->new_connection({
+		    time  => $meta->{time},
+		    saddr => $saddr,
+		    sport => $sport,
+		    daddr => $daddr,
+		    dport => $dport,
+		});
+		if ( ! $obj ) {
+		    # hook says ignore connection
+		    debug("ignoring connection $saddr.$sport -> $daddr.$dport because of hook");
+		    delete $self->{conn}{$saddr,$sport,$daddr,$dport};
+		    return 1;
+		}
+		debug("created object for $saddr.$sport -> $daddr.$dport $obj");
+		$conn->[2] = $obj;
+	    }
+	}
+    }
+
+    return 1;
+}
+
+sub syn { shift->{upper_flow}->syn(@_) }
+sub new_connection { shift->{upper_flow}->new_connection(@_) }
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Net::Inspect::L4::TCP - get IP data, extracts TCP connections
+
+=head1 SYNOPSIS
+
+ my $tcp = Net::Inspect::L4::TCP->new;
+ my $raw = Net::Inspect::L3::IP->new($tcp);
+ $tcp->pktin($data,\%meta);
+
+=head1 DESCRIPTION
+
+Gets IP packets via C<pktin> method and handles connections.
+
+Provides the hooks required by C<Net::Inspect::L3::IP>.
+Usually C<Net::Inspect::Connection::*> is used as upper flow.
+
+Hooks provided:
+
+=over 4
+
+=item pktin($pkt,$meta)
+
+=back
+
+Hooks called on the attached flow object:
+
+=over 4
+
+=item syn(\%meta)
+
+called when the first SYN is received.
+Meta data are saddr, sport, daddr, dport and time.
+If returns false the connection will not be setup, but in this case it will be
+called on the seconds SYN too (because it does not keep track of ignored
+connections).
+
+=item new_connection(\%meta)
+
+will be called if the final ACK for the 3-way handshake is received.
+Must return a connection object or the connection will be ignored.
+Same meta data as in C<syn>.
+
+The connection object will be stored in the flow as long as the connection
+is open. The next hooks will be called on the connection object instead of
+the object attached to the flow.
+
+=back
+
+Methods called on the connection object:
+
+=over 4
+
+=item in($dir,$data,$eof,$time)
+
+Will be called when new data arrived and got acked.
+C<$dir> is the direction of the data (e.g.  0 for data from client, 1 from
+server).
+If C<$eof> is 1 only this direction got closed, on 2 both sides got closed.
+Otherwise C<$eof> is false.
+
+Must return the number of bytes processed from C<$data>. The rest of the data
+will be kept inside the flow object and if new data come in (or FIN gets ACKed)
+the hook will be called again with all unprocessed data.
+
+If C<$eof> is true it should better process all data, because the hook will not
+be called again for this direction.
+
+=item fatal($reason,$time)
+
+Will be called on fatal errors of the connection, e.g. lost packets.
+
+=back
+
+Methods useful for overwriting
+
+=over 4
+
+=item syn(\%meta)
+
+default implementation will just call C<syn> from the attached flow object
+
+=item new_connection(\%meta)
+
+default implementation will just call C<new_connection> from the attached flow
+object
+
+=back
+
+=head1 LIMITS
+
+It will not croak on strange flag combinations.
+It will not cleanup memory for connections, where it never gets both FIN.
